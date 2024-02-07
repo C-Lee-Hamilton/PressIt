987aaa5aafc15469d3cf9e23bbcddeed5ebbdde6
C-Lee-Hamilton <122707378+C-Lee-Hamilton@users.noreply.github.com>
2024-02-06 18:05:10 -0600

    assets

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import "./App.css";
 import { useState, useEffect } from "react";
+import PressMP3 from "./assets/sound/press-it-effect.mp3";
+import PullMP3 from "./assets/sound/pull-effect.mp3";
+import TwistMP3 from "./assets/sound/twist-it-effect.mp3";
+import TwistIMG from "./assets/twistGear.png";
+import PullIMG from "./assets/pulled-1.png";
+import PressInst from "./assets/sound/Press-it.mp3";
+import PullInst from "./assets/sound/Pull-it.mp3";
+import TwistInst from "./assets/sound/twist-it.mp3";
+import LoseMP3 from "./assets/sound/gameover.mp3";
 
 function App() {
   const [twistIt, setTwistIt] = useState(false);
@@ -9,7 +18,23 @@ function App() {
   const [started, setStarted] = useState(false);
   const [score, setScore] = useState(0);
   const [hScore, setHScore] = useState(0);
+  const [pressAudio] = useState(new Audio(PressMP3));
+  const [pullAudio] = useState(new Audio(PullMP3));
+  const [twistAudio] = useState(new Audio(TwistMP3));
+  const [pressInsAudio] = useState(new Audio(PressInst));
+  const [pullInsAudio] = useState(new Audio(PullInst));
+  const [twistInsAudio] = useState(new Audio(TwistInst));
+  const [loseAudio] = useState(new Audio(LoseMP3));
   const moves = ["Twist It!", "Press It!", "Pull It!"];
+  const pressNoise = (e) => {
+    if (e === 1) {
+      pressAudio.play();
+    } else if (e === 2) {
+      pullAudio.play();
+    } else if (e === 3) {
+      twistAudio.play();
+    }
+  };
 
   useEffect(() => {
     if (!started || move === "You Lose") return; // Do not set a timeout if the game hasn't started or is already lost.
@@ -32,6 +57,13 @@ function App() {
   const getRandomMove = () => {
     const randomIndex = Math.floor(Math.random() * moves.length);
     setMove(moves[randomIndex]);
+    // if (move === "Press It!") {
+    //   pressInsAudio.play();
+    // } else if (move === "Pull It!") {
+    //   pullInsAudio.play();
+    // } else if (move === "Twist It!") {
+    //   twistInsAudio.play();
+    // }
   };
 
   const startGame = () => {
@@ -41,6 +73,7 @@ function App() {
     setTwistIt(false);
     setPressIt(false);
     setPullIt(false);
+    pressNoise(1);
   };
 
   const checkAction = (action) => {
@@ -65,6 +98,7 @@ function App() {
 
   const twist = () => {
     if (!started) return;
+    pressNoise(3);
     setTwistIt(true);
     if (move === "Twist It!") {
       checkAction("Twist It!");
@@ -76,6 +110,7 @@ function App() {
   const press = () => {
     if (!started) return;
     setPressIt(true);
+    pressNoise(1);
     if (move === "Press It!") {
       checkAction("Press It!");
     } else {
@@ -86,12 +121,33 @@ function App() {
   const pull = () => {
     if (!started) return;
     setPullIt(true);
+    pressNoise(2);
     if (move === "Pull It!") {
       checkAction("Pull It!");
     } else {
       checkAction("");
     }
   };
+
+  useEffect(() => {
+    // This code will run every time the `move` state changes, which happens after `getRandomMove` is called.
+    if (score) {
+      console.log(`New move is: ${move}`);
+
+      // If you want to play the instructional audio based on the move, uncomment the following lines:
+      if (move === "Press It!") {
+        pressInsAudio.play();
+      } else if (move === "Pull It!") {
+        pullInsAudio.play();
+      } else if (move === "Twist It!") {
+        twistInsAudio.play();
+      }
+    } else if (move) {
+      if (move === "You Lose") {
+        loseAudio.play();
+      }
+    }
+  }, [score, move]);
 
   return (
     <div className="App">
@@ -100,10 +156,7 @@ function App() {
       <h3>Move: {move}</h3>
 
       <div className="twist-container">
-        <h1 className="twist-h1">Twist It</h1>
-        <button className="twist-bar" onClick={twist}>
-          twister
-        </button>
+        <img className="twist-img" onClick={twist} src={TwistIMG} />
       </div>
       <div className="center-press">
         <div className="press-container">
@@ -119,10 +172,7 @@ function App() {
         </div>
       </div>
       <div className="pull-container">
-        <h1 className="pull-h1">Pull It</h1>
-        <button className="pull-bar" onClick={pull}>
-          pull it
-        </button>
+        <img className="pull-img" onClick={pull} src={PullIMG} />
       </div>
     </div>
   );
