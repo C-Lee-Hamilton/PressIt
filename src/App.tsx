d5eb0f2305483a6591abcf8043c69ff5787a91dd
Cody Lee <notlimaheelydoc@gmail.com>
2024-06-06 18:24:12 -0500

    created legacy folder, updated to vite tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,36 @@
-import "./App.css";
-import { useState, useEffect } from "react";
+import { useState,useEffect } from 'react'
 import PressMP3 from "./assets/sound/press-it-effect.mp3";
 import PullMP3 from "./assets/sound/pull-effect.mp3";
 import TwistMP3 from "./assets/sound/twist-it-effect.mp3";
 import TwistIMG from "./assets/twistGear.png";
 import PullIMG from "./assets/pulled-1.png";
+import PullIMG2 from "./assets/pulled-0.png";
 import PressInst from "./assets/sound/Press-it.mp3";
 import PullInst from "./assets/sound/Pull-it.mp3";
 import TwistInst from "./assets/sound/twist-it.mp3";
 import LoseMP3 from "./assets/sound/gameover.mp3";
+import './App.css'
 
 function App() {
-  const [twistIt, setTwistIt] = useState(false);
-  const [pressIt, setPressIt] = useState(false);
-  const [pullIt, setPullIt] = useState(false);
-  const [move, setMove] = useState("");
-  const [started, setStarted] = useState(false);
-  const [score, setScore] = useState(0);
-  const [hScore, setHScore] = useState(0);
-  const [pressAudio] = useState(new Audio(PressMP3));
-  const [pullAudio] = useState(new Audio(PullMP3));
-  const [twistAudio] = useState(new Audio(TwistMP3));
-  const [pressInsAudio] = useState(new Audio(PressInst));
-  const [pullInsAudio] = useState(new Audio(PullInst));
-  const [twistInsAudio] = useState(new Audio(TwistInst));
+  const [twistIt, setTwistIt] = useState<boolean>(false);
+  const [pressIt, setPressIt] = useState<boolean>(false);
+  const [pullIt, setPullIt] = useState<boolean>(false);
+  const [pullAni,setPullAni]=useState<any>(PullIMG2);
+  const [move, setMove] = useState<string>("");
+  const [started, setStarted] = useState<boolean>(false);
+  const [score, setScore] = useState<number>(0);
+  const [hScore, setHScore] = useState<number>(0);
+  const [pressAudio] = useState<any>(new Audio(PressMP3));
+  const [pullAudio] = useState<any>(new Audio(PullMP3));
+  const [twistAudio] = useState<any>(new Audio(TwistMP3));
+  const [pressInsAudio] = useState<any>(new Audio(PressInst));
+  const [pullInsAudio] = useState<any>(new Audio(PullInst));
+  const [twistInsAudio] = useState<any>(new Audio(TwistInst));
   const [loseAudio] = useState(new Audio(LoseMP3));
   const moves = ["Twist It!", "Press It!", "Pull It!"];
-  const pressNoise = (e) => {
+
+
+  const pressNoise = (e:number) => {
     if (e === 1) {
       pressAudio.play();
     } else if (e === 2) {
@@ -57,13 +61,13 @@ function App() {
   const getRandomMove = () => {
     const randomIndex = Math.floor(Math.random() * moves.length);
     setMove(moves[randomIndex]);
-    // if (move === "Press It!") {
-    //   pressInsAudio.play();
-    // } else if (move === "Pull It!") {
-    //   pullInsAudio.play();
-    // } else if (move === "Twist It!") {
-    //   twistInsAudio.play();
-    // }
+    if (move === "Press It!") {
+      pressInsAudio.play();
+    } else if (move === "Pull It!") {
+      pullInsAudio.play();
+    } else if (move === "Twist It!") {
+      twistInsAudio.play();
+    }
   };
 
   const startGame = () => {
@@ -76,16 +80,16 @@ function App() {
     pressNoise(1);
   };
 
-  const checkAction = (action) => {
+  const checkAction = (action:any) => {
     if (action === move) {
       setScore((prevScore) => prevScore + 1);
       if (score + 1 > hScore) {
         setHScore(score + 1);
       }
       getRandomMove();
-      setTwistIt(false);
-      setPressIt(false);
-      setPullIt(false);
+      // setTwistIt(false);
+      // setPressIt(false);
+      // setPullIt(false);
     } else {
       setStarted(false);
       setMove("You Lose");
@@ -98,6 +102,8 @@ function App() {
 
   const twist = () => {
     if (!started) return;
+    setPressIt(false);
+    setPullIt(false);
     pressNoise(3);
     setTwistIt(true);
     if (move === "Twist It!") {
@@ -109,6 +115,8 @@ function App() {
 
   const press = () => {
     if (!started) return;
+    setTwistIt(false);
+    setPullIt(false);
     setPressIt(true);
     pressNoise(1);
     if (move === "Press It!") {
@@ -120,8 +128,14 @@ function App() {
 
   const pull = () => {
     if (!started) return;
+     setTwistIt(false);
+      setPressIt(false);
     setPullIt(true);
     pressNoise(2);
+    setPullAni(PullIMG);
+    setTimeout(() => {
+      setPullAni(PullIMG2);
+    }, 500);
     if (move === "Pull It!") {
       checkAction("Pull It!");
     } else {
@@ -151,12 +165,13 @@ function App() {
 
   return (
     <div className="App">
+   
       <h4>Score: {score}</h4>
       <h4>High Score: {hScore}</h4>
       <h3>Move: {move}</h3>
 
-      <div className="twist-container">
-        <img className="twist-img" onClick={twist} src={TwistIMG} />
+      <div className="twist-container" >
+        <img className={twistIt===true ? "twist-spin" : "twist-img"}    onClick={twist} src={TwistIMG} />
       </div>
       <div className="center-press">
         <div className="press-container">
@@ -172,10 +187,12 @@ function App() {
         </div>
       </div>
       <div className="pull-container">
-        <img className="pull-img" onClick={pull} src={PullIMG} />
+        <img className="pull-img"
+        //  style={{height: pullIt ? "210px": "150px"}}
+         onClick={pull} src={pullAni} />
       </div>
     </div>
-  );
+  )
 }
 
 export default App;
